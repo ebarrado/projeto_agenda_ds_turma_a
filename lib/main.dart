5e9e86e77aa40dfcd7df64ba04506e006e442c06
ebarrado <erika_barrado@hotmail.com>
2024-10-10 07:53:27 -0300

    Pagina Agenda com Modal Cadastrar Atividade e Formulario no Modal

--- lib/main.dart
+++ lib/main.dart
@@ -22,50 +22,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class PaginaAgenda extends StatelessWidget {
+class PaginaAgenda extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Agenda - Barrado',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 12.0,
-            ),
-          ),
-        ),
-        backgroundColor: Color.fromARGB(255, 134, 3, 10),
-      ),
-      backgroundColor: Color.fromARGB(255, 243, 239, 240),
-      body: ListView(
-        //scrollDirection: Axis.horizontal,
-        children: [
-          Atividade('Atividade 1',
-              'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png'),
-          Atividade('Atividade 2',
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQexxwaG6qMQt_I6TW_ezMQu3suyMWQPKmNgQ&s'),
-          Atividade('Atividade 3',
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn7XprTV9T6eic0nFpkS4RziT8pOa2nbJ2jw&s'),
-          Atividade('Atividade 4',
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_uLYwsfqtf-Ax5N48y1w3J7IA07XxU-eGVA&s')
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // modalCadastrar(context);
-          showAboutDialog(context: context);
-          print('botão pressionado');
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
+  State<PaginaAgenda> createState() => _PaginaAgendaState();
 }
 
-class CadastrarAtividade extends StatefulWidget {
-  const CadastrarAtividade({super.key});
+class _PaginaAgendaState extends State<PaginaAgenda> {
+  //variavel do tipo final
+  final List<Map<String, String>> _atividades = [];
 
   //MÉTODO CADASTRAR MODAL
   //Método do Botão
@@ -136,13 +100,42 @@ class CadastrarAtividade extends StatefulWidget {
   }
 
   @override
-  State<CadastrarAtividade> createState() => _CadastrarAtividadeStatus();
-}
-
-class _CadastrarAtividadeStatus extends State<CadastrarAtividade> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Agenda - Barrado',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12.0,
+            ),
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 134, 3, 10),
+      ),
+      backgroundColor: Color.fromARGB(255, 243, 239, 240),
+      body: ListView(
+        //scrollDirection: Axis.horizontal,
+        children: [
+          Atividade('Atividade 1',
+              'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png'),
+          Atividade('Atividade 2',
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQexxwaG6qMQt_I6TW_ezMQu3suyMWQPKmNgQ&s'),
+          Atividade('Atividade 3',
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn7XprTV9T6eic0nFpkS4RziT8pOa2nbJ2jw&s'),
+          Atividade('Atividade 4',
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_uLYwsfqtf-Ax5N48y1w3J7IA07XxU-eGVA&s')
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          modalCadastrar(context);
+          print('botão pressionado');
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
 
